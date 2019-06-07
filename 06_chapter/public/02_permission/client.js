0fbb90c1054aa029903b8efb15176260b28e6777
叶亮 <yeliang@tv365.net>
2019-06-08 00:57:55 +0800

    添加录制视频功能和保存到本地功能

--- 06_chapter/public/02_permission/client.js
+++ 06_chapter/public/02_permission/client.js
@@ -13,11 +13,21 @@ var picture = document.querySelector('canvas#picture');
 picture.width = 320;
 picture.height = 240;
 
+//用于显示视频流参数信息
 var divConstraints = document.querySelector('div#constraints');
 
-//后去到video标签
+//获取到video标签
 var videoplay = document.querySelector('video#player');
-var audioplay = document.querySelector('audio#audioplayer');
+//var audioplay = document.querySelector('audio#audioplayer');
+
+//录制相关
+var recvideo = document.querySelector('video#recplayer');
+var btnRecord = document.querySelector('button#record');
+var btnPlay = document.querySelector('button#recplay');
+var btnDownload = document.querySelector('button#download');
+
+var buffer;
+var mediaRecorder;
 
 //将流赋值给video标签
 function gotMediaStream(stream){
@@ -28,9 +38,8 @@ function gotMediaStream(stream){
 	var videoTrack = stream.getVideoTracks()[0];
 	var videoConstraints = videoTrack.getSettings();
 
-
 	divConstraints.textContent = JSON.stringify(videoConstraints, null, 2);
-
+	window.stream = stream;
 	return navigator.mediaDevices.enumerateDevices();
 }
 
@@ -67,8 +76,8 @@ function start(){
 		var constraints = {
 			 video : {
 				//修改视频宽高
-				width : 640,
-				height : 480,
+				width : 320,
+				height : 240,
 
 				//设置帧率
 				frameRate : 15,
@@ -103,3 +112,69 @@ snapshort.onclick = function(){
 	picture.getContext('2d').drawImage(videoplay, 0,0, picture.width,picture.height);
 }
 
+function handleDataAvailable(e){
+	if(e && e.data && e.data.size > 0){
+		buffer.push(e.data);
+	}
+}
+
+function startRecord(){
+	buffer = [];
+	var options = {
+		mimeType : 'video/webm; codecs = vp8'
+	}
+
+	if(!MediaRecorder.isTypeSupported(options.mimeType)){
+			console.error('${options.mimeType} is not supported!');
+			return;
+	}
+
+	try{
+		mediaRecorder = new MediaRecorder(window.stream, options);
+	}catch(e){
+		console.error('Failed to create MediaRecorder:',e);
+		return
+	}
+	mediaRecorder.ondataavailable = handleDataAvailable;
+	mediaRecorder.start(10);
+}
+
+function stopRecord(){
+	mediaRecorder.stop();
+}
+
+//录制按钮监听
+btnRecord.onclick = ()=>{
+	if(btnRecord.textContent === 'Start Record'){
+		startRecord();
+		btnRecord.textContent = 'Stop Record';
+		btnPlay.disabled = true;
+		btnDownload.disabled = true;
+	}else{
+		stopRecord();
+		btnRecord.textContent = 'Start Record';
+		btnPlay.disabled = false;
+		btnDownload.disabled = false;
+	}
+}
+
+//播放按钮监听
+btnPlay.onclick = ()=>{
+	var blob = new Blob(buffer, {type : 'video/webm'});
+	recvideo.src = window.URL.createObjectURL(blob);
+	recvideo.srcObject = null;
+	recvideo.controls = true;
+	recvideo.play();
+}
+
+//下载按钮监听
+btnDownload.onclick = ()=>{
+	var blob = new Blob(buffer, {type: 'video/webm'});
+	var url = window.URL.createObjectURL(blob);
+	var a = document.createElement('a');
+
+	a.href = url;
+	a.style.display = 'none';
+	a.download = 'aaa.webm';
+	a.click();
+}
